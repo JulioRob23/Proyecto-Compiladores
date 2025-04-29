e68069d530bcd3966e4f23b28dadc832468cc4ba
Andrea <andreagiboy@gmail.com>
2025-04-29 09:55:18 -0600

    implementacion visit de: else, cond, condv, act, arith, xerneas y uxie

--- Fase 2/Evaluador.py
+++ Fase 2/Evaluador.py
@@ -38,6 +38,8 @@ class Evaluador(proyectoVisitor):
 
     # Visit a parse tree produced by proyectoParser#else.
     def visitElse(self, ctx:proyectoParser.ElseContext):
+        if ctx.CLEFABLE() is not None:
+            print("Visit ELSE - Value: CLEFABLE")
         return self.visitChildren(ctx)
 
 
@@ -48,41 +50,103 @@ class Evaluador(proyectoVisitor):
 
     # Visit a parse tree produced by proyectoParser#cond.
     def visitCond(self, ctx:proyectoParser.CondContext):
-        return self.visitChildren(ctx)
+        if ctx.EXC() is not None and ctx.EQUAL() is not None:
+            print("Visit COND - Value: EXC EQUAL")
+        elif ctx.EQUAL() is not None:
+            print("Visit COND - Value: EQUAL")
+        elif ctx.MINOR() is not None and ctx.EQUAL() is not None:
+            print("Visit COND - Value: MINOR EQUAL")
+        elif ctx.BIGGER() is not None and ctx.EQUAL() is not None:
+            print("Visit COND - Value: BIGGER EQUAL")
+        elif ctx.MINOR() is not None:
+            print("Visit COND - Value: MINOR")
+        elif ctx.BIGGER() is not None:
+            print("Visit COND - Value: BIGGER")
+        else:
+            print("Visit COND - Error: Operador desconocido")
+        return
 
 
     # Visit a parse tree produced by proyectoParser#condv.
     def visitCondv(self, ctx:proyectoParser.CondvContext):
-        return self.visitChildren(ctx)
+        if ctx.EXC() is not None and ctx.EQUAL() is not None:
+            print("Visit CONDV - Value: EXC EQUAL")
+        elif ctx.EQUAL() is not None:
+            print("Visit CONDV - Value: EQUAL")
+        return
 
 
     # Visit a parse tree produced by proyectoParser#act.
     def visitAct(self, ctx:proyectoParser.ActContext):
-        return self.visitChildren(ctx)
+        if ctx.UNOWN() is not None:
+            print("Visit ACT - value: Unown")
+        return self.visit(ctx.line())
 
 
     # Visit a parse tree produced by proyectoParser#arith.
     def visitArith(self, ctx:proyectoParser.ArithContext):
-        return self.visitChildren(ctx)
+        if ctx.ID() is not None and ctx.EQUAL() is not None:
+            print(f"Visit ARITH - value: {ctx.ID().getText().strip()}")
+            self.visit(ctx.xerneas(0))
+        for i in range(1, len(ctx.xerneas())):
+            if ctx.PLUS() is not None:
+                operador = "+"
+            elif ctx.MINUS() is not None:
+                operador = "-"
+            else:
+                operador = "ninguno"  
+            print(f"Visit ARITH - value: {operador}")
+            self.visit(ctx.xerneas(i))     
+        return
 
 
     # Visit a parse tree produced by proyectoParser#xerneas.
     def visitXerneas(self, ctx:proyectoParser.XerneasContext):
-        return self.visitChildren(ctx)
+         print("Visit XERNEAS - value: uxie")
+         print("Visit XERNEAS - value: uxie")
+         self.visit(ctx.uxie(0))
+         for i in range(1, len(ctx.uxie())):
+            if ctx.MUL() is not None:
+                operador = "*"
+            elif ctx.DIV() is not None:
+                operador = "/"
+            else:
+                operador = "ninguno" 
+            
+            if operador:
+                print(f"Visit XERNEAS - value: {operador}")
+            
+            print("Visit XERNEAS - value: uxie")
+         return 
 
 
     # Visit a parse tree produced by proyectoParser#uxie.
     def visitUxie(self, ctx:proyectoParser.UxieContext):
-        return self.visitChildren(ctx)
+        if ctx.OPA() is not None:
 
+            print("Visit UXIE - Value: Paréntesis abierto")
+            self.visit(ctx.arith())
+            print("Visit UXIE - Paréntesis cerrado")
+        elif ctx.INT() is not None:
+            print(f"Visit UXIE - Value: {int(ctx.INT().getText().strip())}")
+        elif ctx.ID() is not None:
+            print(f"Visit UXIE - Value: {ctx.ID().getText().strip()}")
+        else: 
+            print("Error, invalid value")
+        return 
 
     # Visit a parse tree produced by proyectoParser#func.
     def visitFunc(self, ctx:proyectoParser.FuncContext):
         return self.visitChildren(ctx)
 
 
-    # Visit a parse tree produced by proyectoParser#extra.
-    def visitExtra(self, ctx:proyectoParser.ExtraContext):
+    # Visit a parse tree produced by proyectoParser#extraf.
+    def visitExtraf(self, ctx:proyectoParser.ExtrafContext):
+        return self.visitChildren(ctx)
+    
+
+      # Visit a parse tree produced by proyectoParser#extrac.
+    def visitExtrac(self, ctx:proyectoParser.ExtracContext):
         return self.visitChildren(ctx)
 
 
